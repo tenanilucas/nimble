d3ed5a7299b2033a9bfe2e232eb57fc60ef92605
Lucas Tenani <lucas.tenani@espacolaser.com>
2021-11-01 11:07:45 -0300

    Sidebar das aprovacoes de documentos foram criadas

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -1,12 +1,42 @@
 import React from "react";
 import NavBar from "../../components/NavBar";
+import * as AiIcons from "react-icons/ai";
+import * as MdIcons from "react-icons/md";
+import "./styles.scss";
 
 function App() {
   return (
     <>
       <NavBar />
       <main>
-        <h1>App</h1>
+        <div className="main__body">
+          <div className="sidebar__document">
+            <button className="sidebar__compose">
+              <AiIcons.AiOutlinePlus className="material-icons" />
+              Compose
+            </button>
+            <div className="sidebarOption sidebarOption__active">
+              <AiIcons.AiOutlineInbox />
+              <h3>Inbox</h3>
+            </div>
+            <div className="sidebarOption ">
+              <AiIcons.AiOutlineStar />
+              <h3>Starred</h3>
+            </div>
+            <div className="sidebarOption ">
+              <MdIcons.MdOutlineWatchLater />
+              <h3>Snoozed</h3>
+            </div>
+            <div className="sidebarOption ">
+              <MdIcons.MdLabelImportantOutline />
+              <h3>Important</h3>
+            </div>
+            <div className="sidebarOption ">
+              <AiIcons.AiOutlineSend />
+              <h3>Sent</h3>
+            </div>
+          </div>
+        </div>
       </main>
     </>
   );
