3cf83c02e1933425415c1b818ac330670b8adec2
Lucas Tenani <lucas.tenani@espacolaser.com>
2021-12-13 18:04:15 -0300

    Now if the user is idle for a long time he will be disconnected

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -1,7 +1,23 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
+import { Route, Redirect, useHistory } from "react-router-dom";
+import api from "./services/api";
 
 const PrivateRoute = (props) => {
+  const history = useHistory();
+
+  api.interceptors.response.use(
+    (response) => {
+      return response;
+    },
+    async function (error) {
+      if (error.response.status === 401) {
+        alert("Sessão expirada");
+        history.push("/login");
+      }
+      return Promise.reject(error);
+    }
+  );
+
   const isLogged = !!localStorage.getItem("@token");
   return isLogged ? <Route {...props} /> : <Redirect to="/login" />;
 };
