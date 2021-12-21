6345008f84611b9dc23a9a42c1738fc8d1293072
Lucas Tenani <lucas.tenani@espacolaser.com>
2021-12-21 17:51:59 -0300

    Inserindo estilo de tabela em uma pasta para ser utilizado por mais de uma página

--- src/pages/Team/index.js
+++ src/pages/Team/index.js
@@ -8,42 +8,36 @@ function Team() {
       <NavBar />
       <main>
         <section className="container section section-height">
-          <div class="limiter">
-            <div class="container-table100">
-              <div class="wrap-table100">
-                <div class="table100">
-                  <table>
-                    <thead>
-                      <tr class="table100-head">
-                        <th class="column1">Nome</th>
-                        <th class="column2">Superior</th>
-                        <th class="column3">Unidade</th>
-                        <th class="column4">Está na equipe?</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
-                        <td class="column1">Luiz</td>
-                        <td class="column2">Felipe</td>
-                        <td class="column3">São Paulo</td>
-                        <td class="column4">
-                          <button className="myButton no">NÃO</button>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td class="column1">João</td>
-                        <td class="column2">Felipe</td>
-                        <td class="column3">São Paulo</td>
-                        <td class="column4">
-                          <button className="myButton yes">SIM</button>
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
-                </div>
-              </div>
-            </div>
-          </div>
+          <h1 className="section__title">Time</h1>
+
+          <table>
+            <thead>
+              <tr class="table100-head">
+                <th class="column1">Nome</th>
+                <th class="column2">Superior</th>
+                <th class="column3">Unidade</th>
+                <th class="column4">Está na equipe?</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td class="column1">Luiz</td>
+                <td class="column2">Felipe</td>
+                <td class="column3">São Paulo</td>
+                <td class="column4">
+                  <button className="myButton no">NÃO</button>
+                </td>
+              </tr>
+              <tr>
+                <td class="column1">João</td>
+                <td class="column2">Felipe</td>
+                <td class="column3">São Paulo</td>
+                <td class="column4">
+                  <button className="myButton yes">SIM</button>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </section>
       </main>
     </>
