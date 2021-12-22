8788cd3df54261e5a53ee722290600d993883849
Lucas Tenani <lucas.tenani@espacolaser.com>
2021-12-22 12:49:58 -0300

    Foi criada uma condição para quando não houver documentos

--- src/pages/App/index.js
+++ src/pages/App/index.js
@@ -22,36 +22,42 @@ function App() {
       <main>
         {documents ? (
           <section className="container section section-height">
-            <h1 className="section__title">Aprovação</h1>
-            <div className="details">
-              <div className="recentOrders">
-                <div className="cardHeader"></div>
-                <table>
-                  <thead>
-                    <tr>
-                      <td></td>
-                      <td>Número</td>
-                      <td>Fornecedor</td>
-                      <td>Filial</td>
-                      <td>Valor</td>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {documents?.Documentos.map((document, index) => (
-                      <tr key={index}>
-                        <td>
-                          <input type="checkbox" />
-                        </td>
-                        <td>{document.NUM_SOLICIT}</td>
-                        <td>{document.NOME_FORNEC}</td>
-                        <td>{document.FILIAL}</td>
-                        <td>R$ {document.VALOR_TOTAL}</td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-            </div>
+            {documents.Documentos.length === 0 ? (
+              <h1>Não há documentos para serem mostradas no momento</h1>
+            ) : (
+              <>
+                <h1 className="section__title">Aprovação</h1>
+                <div className="details">
+                  <div className="recentOrders">
+                    <div className="cardHeader"></div>
+                    <table>
+                      <thead>
+                        <tr id="navHeader">
+                          <td></td>
+                          <td>Número</td>
+                          <td>Fornecedor</td>
+                          <td>Filial</td>
+                          <td>Valor</td>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {documents?.Documentos.map((document, index) => (
+                          <tr key={index}>
+                            <td>
+                              <input type="checkbox" />
+                            </td>
+                            <td>{document.NUM_SOLICIT}</td>
+                            <td>{document.NOME_FORNEC}</td>
+                            <td>{document.FILIAL}</td>
+                            <td>R$ {document.VALOR_TOTAL}</td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
+                </div>
+              </>
+            )}
           </section>
         ) : (
           <Loading />
