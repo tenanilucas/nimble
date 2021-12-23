c7097d31844f7c8d2c59537d7d26822d5f1c8dcd
Lucas Tenani <lucas.tenani@espacolaser.com>
2021-12-23 16:15:09 -0300

    Resolvendo mais alguns problemas de responsividade

--- src/pages/Team/index.js
+++ src/pages/Team/index.js
@@ -9,35 +9,39 @@ function Team() {
       <main>
         <section className="container section section-height">
           <h1 className="section__title">Time</h1>
-
-          <table>
-            <thead>
-              <tr class="table100-head">
-                <th class="column1">Nome</th>
-                <th class="column2">Superior</th>
-                <th class="column3">Unidade</th>
-                <th class="column4">Está na equipe?</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td class="column1">Luiz</td>
-                <td class="column2">Felipe</td>
-                <td class="column3">São Paulo</td>
-                <td class="column4">
-                  <button className="myButton no">NÃO</button>
-                </td>
-              </tr>
-              <tr>
-                <td class="column1">João</td>
-                <td class="column2">Felipe</td>
-                <td class="column3">São Paulo</td>
-                <td class="column4">
-                  <button className="myButton yes">SIM</button>
-                </td>
-              </tr>
-            </tbody>
-          </table>
+          <div className="details">
+            <div className="recentOrders">
+              <div className="cardHeader"></div>
+              <table>
+                <thead>
+                  <tr id="navHeader">
+                    <th>Nome</th>
+                    <th>Superior</th>
+                    <th>Unidade</th>
+                    <th>Está na equipe?</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>Luiz</td>
+                    <td>Felipe</td>
+                    <td>São Paulo</td>
+                    <td>
+                      <button>NÃO</button>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td>João</td>
+                    <td>Felipe</td>
+                    <td>São Paulo</td>
+                    <td>
+                      <button>SIM</button>
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </section>
       </main>
     </>
